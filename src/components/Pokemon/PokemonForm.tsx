15575907f4e02eb04c7ef8bf09c8b5326983181f
BuntyBru <buntybru@gmail.com>
2022-12-03 19:14:07 +0530

    done

--- src/components/Pokemon/PokemonForm.tsx
+++ src/components/Pokemon/PokemonForm.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import fetchPokemon from "../../api/fetchPokemon";
+import { PokemonDataType } from "../../type/pokemon";
 
 const StyledFormContainer = styled.div`
   display: flex;
   justify-contemnt: center;
+  flex-direction: column;
+  gap: 10px;
   form {
     display: flex;
     flex-direction: column;
@@ -18,16 +21,34 @@ const StyledFormContainer = styled.div`
   }
 `;
 
-const StyledDetailContainer = styled.div``;
+const StyledDetailContainer = styled.div`
+  display: flex;
+  .image-section {
+    height: 50px;
+    width: 50px;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+  }
+  .details-section {
+    flex: 2;
+  }
+  p {
+    margin: 0px;
+  }
+`;
 
 const PokemonForm = () => {
   const [pokemonName, setPokemonName] = React.useState<string>("");
-  //const [pokemonDetails,setPokemonDetails] = React.useState<>
+  const [pokemonDetails, setPokemonDetails] =
+    React.useState<PokemonDataType | null>(null);
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     fetchPokemon(pokemonName).then((pokemonData) => {
       console.log(pokemonData);
+      setPokemonDetails(pokemonData);
     });
   };
   return (
@@ -44,7 +65,13 @@ const PokemonForm = () => {
       </form>
 
       <StyledDetailContainer>
-        <p>{}</p>
+        <div className="image-section">
+          <img src={pokemonDetails?.image} alt={pokemonDetails?.name} />
+        </div>
+        <div className="details-section">
+          <p>{pokemonDetails?.name}</p>
+          <p>{pokemonDetails?.number}</p>
+        </div>
       </StyledDetailContainer>
     </StyledFormContainer>
   );
--- src/type/pokemon.tsx
+++ src/type/pokemon.tsx
@@ -1,3 +1,18 @@
 export type HeadersHTTPType = {
   "content-type": string;
 };
+
+export type SpecialAttackData = {
+  damage: number;
+  name: string;
+  type: string;
+};
+
+export type PokemonDataType = {
+  attacks: { special: Array<SpecialAttackData> };
+  fetchedAt: "18:58 26.423";
+  id: "UG9rZW1vbjowMjU=";
+  image: "https://img.pokemondb.net/artwork/pikachu.jpg";
+  name: "Pikachu";
+  number: "025";
+};
